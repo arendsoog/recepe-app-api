b2f3fe22a4d9d9ed09d7e84d4c321e524c67b8b1
Joan Van Berlo <joan.van.berlo@gmail.com>
2022-10-20 11:47:01 +0200

    add tags to recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -10,13 +10,37 @@ from core.models import (
 )
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer fro tags"""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(serializers.ModelSerializer):
@@ -24,12 +48,3 @@ class RecipeDetailSerializer(serializers.ModelSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer fro tags"""
-
-    class Meta:
-        model = Tag
-        fiels = ['id', 'name']
-        read_only_fields = ['id']
--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -17,6 +17,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user"""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -39,7 +44,7 @@ class PrivateTagsApiTest(TestCase):
     """Test authenticated API requests."""
 
     def setUp(self):
-        self.usser = create_user()
+        self.user = create_user()
         self.client = APIClient()
         self.client.force_authenticate(self.user)
 
@@ -68,3 +73,27 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_teg(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
